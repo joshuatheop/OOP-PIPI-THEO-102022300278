a10d786b448f4fef8ce73f37c779623b31932b5f
joshuatheop <152384540+joshuatheop@users.noreply.github.com>
2024-12-12 08:36:51 +0700

    Jurnal Modul 3
    Jurnal susah

---
+++ MODUL3/MODUL3_THEO/src/MainAppController.java
@@ -0,0 +1,171 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.MissingFormatArgumentException;
+
+public class MainAppController {
+
+    @FXML
+    private TableView<Task> taskTable; // Tabel untuk menampilkan daftar tugas
+    @FXML
+    private TableColumn<Task, String> titleCol, priorityCol, statusCol; // Kolom untuk judul, prioritas, dan status tugas
+    @FXML
+    private TableColumn<Task, LocalDate> dueDateCol; // Kolom untuk tanggal jatuh tempo
+    @FXML
+    private TextField titleField; // Field untuk memasukkan judul tugas
+    @FXML
+    private ComboBox<String> priorityBox; // ComboBox untuk memilih prioritas
+    @FXML
+    private DatePicker dueDatePicker; // DatePicker untuk memilih tanggal jatuh tempo
+
+    private ObservableList<Task> taskList; // Daftar tugas yang dapat diamati
+
+    @FXML
+    public void initialize() {
+        taskList = FXCollections.observableArrayList(TaskFileManager.loadTasks());
+
+        // TODO: konfigurasi kolom tabel untuk setiap atribut task
+        titleCol.setCellValueFactory(new PropertyValueFactory<>("title"));
+        priorityCol.setCellValueFactory(new PropertyValueFactory<>("priority"));
+        dueDateCol.setCellValueFactory(new PropertyValueFactory<>("dueDate"));
+        statusCol.setCellValueFactory(new PropertyValueFactory<>("status"));
+        // hint: gunakan setCellValueFactory dan PropertyValueFactory
+
+        // TODO: konfigurasi lebar kolom agar lebih rapi
+        titleCol.setPrefWidth(160);
+        priorityCol.setPrefWidth(160);
+        dueDateCol.setPrefWidth(160);
+        statusCol.setPrefWidth(160);
+        // hint: gunakan setPrefWidth
+
+        taskTable.setItems(taskList);
+
+        // TODO: Tambahkan options prioritas ke ComboBox
+        priorityBox.getItems().addAll("High", "Medium", "Low");
+    }
+
+    @FXML
+    private void addTask() {
+        try {
+        
+            if (titleField.getText().isEmpty() || priorityBox.getValue() == null || dueDatePicker.getValue() == null) {
+                // TODO: Throw new MissingFieldException dengan pesan yang sesuai
+                throw new MissingFieldException("Yang lu isi gada bang");
+            }
+
+            if (dueDatePicker.getValue().isBefore(LocalDate.now())) {
+                // TODO: Throw new InvalidDateException dengan pesan yang sesuai 
+                throw new InvalidDateException("Yang lu masukin ga bener bang");
+            }
+
+            // TODO: Buat objek task baru dengan value dari field pada form
+            // hint: gunakan getText() untuk TextField, getValue() untuk ComboBox, dan DatePicker, serta masukkan status "Incomplete" secara default
+            Task task = new Task(
+                titleField.getText(),
+                priorityBox.getValue(),
+                dueDatePicker.getValue(),
+                "Incomplete"
+            );
+
+            // TODO: Tambahkan objek task yang telah dibuat ke taskList
+            // hint: gunakan function bawaan dari ObservableList, yaitu add
+            taskList.add(task);
+            clearData();
+            TaskFileManager.saveTasks(new ArrayList<>(taskList));
+        }
+        
+        // Contoh catch MissingFieldException dan InvalidDateException
+        catch (MissingFieldException | InvalidDateException e) {
+            showError(e.getMessage());
+        }
+
+    }
+
+    // TODO: hapus semua value dari field pada form
+    // hint: gunakan function getSelectionModel().clearSelection() untuk ComboBox dan getEditor().clear() untuk DatePickea
+    private void clearData(){
+
+    }
+
+    @FXML
+    private void deleteTask() {
+        try {
+    
+            Task selected = taskTable.getSelectionModel().getSelectedItem();
+            if (selected == null) {
+                // TODO: Throw new TaskNotSelectedException dengan pesan yang sesuai 
+                throw new TaskNotSelectedException("Lu blom milih bang");
+                
+            }
+
+            taskList.remove(selected);
+            TaskFileManager.saveTasks(new ArrayList<>(taskList));
+        }
+        
+        // TODO: Catch TaskNotSelectedException dan panggil showError (Lihat contoh catch)
+        catch (TaskNotSelectedException e){
+            showError(e.getMessage());;
+        }
+    }
+
+    @FXML
+    private void markTaskComplete() {
+        try {
+            Task selected = taskTable.getSelectionModel().getSelectedItem();
+            if (selected == null) {
+                // TODO: Throw new TaskNotSelectedException dengan pesan yang sesuai 
+                throw new TaskNotSelectedException("Lu ga milih apa apa bang");
+
+            }
+            // TODO: Ubah status task menjadi "Complete"
+            // hint: gunakan function setStatus dari kelas Task
+            selected.setStatus("Complete");
+
+            taskTable.refresh();
+            TaskFileManager.saveTasks(new ArrayList<>(taskList));
+        }
+
+        // TODO: Catch TaskNotSelectedException dan panggil showError sekali lagi
+        catch (TaskNotSelectedException e){
+            showError(e.getMessage());;
+        }
+        
+
+    }
+
+    private void showError(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR, message, ButtonType.OK);
+        alert.showAndWait();
+    }
+
+    // TODO: Buat kelas MissingFieldException
+    public class MissingFieldException extends Exception{
+        public MissingFieldException(String Message) {
+            super(Message);
+        }
+    }
+
+    // TODO: Buat kelas TaskNotSelectedException
+    public class TaskNotSelectedException extends Exception{
+        public TaskNotSelectedException(String Message) {
+            super(Message);
+        }
+    }
+    // TODO: Buat kelas InvalidDateException
+    public class InvalidDateException extends Exception{
+        public InvalidDateException(String Message) {
+            super(Message);
+        }
+    }
+    // TODO: Buat kelas MissingDateException
+    public class MissingDateException extends Exception{
+        public MissingDateException(String Message) {
+            super(Message);
+    }
+}
+}
